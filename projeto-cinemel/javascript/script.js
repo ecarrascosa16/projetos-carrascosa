7ead1d5f7f7e82f234069b4eefd34d9da9b66c1a
Eduardo Carrascosa <eduardoc.codes@gmail.com>
2024-12-25 03:08:15 -0300

    aprimorando a tela login e cadastro
    removi as paginas de Login e cadastro e agora elas aparecem como bump

--- projeto-cinemel/javascript/script.js
+++ projeto-cinemel/javascript/script.js
@@ -17,3 +17,27 @@ function carregarNome() {
 if (document.getElementById("nomeusuario")) {
     carregarNome();
 }
+
+
+let painel_login = document.getElementById('painel-login-fundo')
+let painel_cadastro = document.getElementById('painel-cadastro-fundo')
+
+function mostrarLogin() {
+    painel_login.style.display = 'flex'
+    painel_cadastro.style.display = 'none'
+}
+
+
+
+function fecharLogin() {
+    painel_login.style.display = 'none'
+}
+
+function mostrarCadastro() {
+    painel_cadastro.style.display = 'flex'
+    painel_login.style.display = 'none'
+}
+
+function fecharCadastro() {
+    painel_cadastro.style.display = 'none'
+}
