76da5501c0d66cece8fa46ec365ca5e1cf7cc7de
Eduardo Carrascosa <eduardoc.codes@gmail.com>
2025-01-09 21:42:44 -0300

    carrosel na pagina sports
    fiz o carrossel usando a biblioteca SWIPER

--- projeto-cinemel/javascript/script.js
+++ projeto-cinemel/javascript/script.js
@@ -1,24 +1,43 @@
-let painel_login = document.getElementById('painel-login-fundo')
-let painel_cadastro = document.getElementById('painel-cadastro-fundo')
+let painel_login = document.getElementById("painel-login-fundo");
+let painel_cadastro = document.getElementById("painel-cadastro-fundo");
 
 function mostrarLogin() {
-    painel_login.style.display = 'flex'
-    painel_cadastro.style.display = 'none'
+  painel_login.style.display = "flex";
+  painel_cadastro.style.display = "none";
 }
 
-
-
 function fecharLogin() {
-    painel_login.style.display = 'none'
+  painel_login.style.display = "none";
 }
 
 function mostrarCadastro() {
-    painel_cadastro.style.display = 'flex'
-    painel_login.style.display = 'none'
+  painel_cadastro.style.display = "flex";
+  painel_login.style.display = "none";
 }
 
 function fecharCadastro() {
-    painel_cadastro.style.display = 'none'
+  painel_cadastro.style.display = "none";
 }
 
+// INICIALIZAÇÃO DO SWIPER
 
+let swiper = new Swiper(".swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+        slidesPerView: 4,
+        spaceBetween: 1
+        },
+        200: {
+        slidesPerView: 4,
+        spaceBetween: 1
+        },
+        1188: {
+        slidesPerView: 7,
+        spaceBetween: 1
+        }
+    }
+  });
