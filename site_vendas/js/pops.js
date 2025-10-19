1197804990ba410aaae4de3fa65d09ab1c903bfd
Eduardo Carrascosa <eduardoc.codes@gmail.com>
2025-10-19 15:07:03 -0300

    atualizei um monte de coisa

---
+++ site_vendas/js/pops.js
@@ -0,0 +1,13 @@
+
+
+function abrirPopCineMel() {
+    const popCineMel = document.querySelector("#pop-cinemel");
+
+    popCineMel.style.display = 'block';
+} 
+
+function fecharPopCineMel() {
+    const popCineMel = document.querySelector("#pop-cinemel");
+
+    popCineMel.style.display = 'none';
+} 
